bdb8d37a33f2752d1addf6f43bf679389d47ba75
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-05-26 15:44:03 +0200

    first-commit

---
+++ src/proxy.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server";
+
+export const runtime = 'nodejs'
+
+export function proxy(request: NextRequest) {}
+
+export const config = {
+  matcher: [
+    '/dashboard/:path*',
+    '/admin/:path*',
+    '/api/ingest',
+    '/api/email',
+    '/api/locations',
+  ],
+}
