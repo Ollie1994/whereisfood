5a7bcca30c42b1344c75ad66c6b0709b15103b40
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-06-27 14:37:50 +0200

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import 'maplibre-gl/dist/maplibre-gl.css'
+import "maplibre-gl/dist/maplibre-gl.css";
 import "./globals.css";
 
 const geistSans = Geist({
